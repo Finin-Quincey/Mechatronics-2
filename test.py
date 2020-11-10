19b5da03db38516a180f92bbead4922bd026510c
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2020-11-10 16:09:28 +0000

    Argsort test

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 import cv2
 from cv2 import aruco
+import numpy
 
 ham = 1
 spam = [3, 4]
 eggs = "monty"
 
 print("Hello world!")
+
+randoms = [5, 8, 1, 4]
+
+print(numpy.argsort(randoms))
+
+for i in numpy.argsort(randoms):
+    print(randoms[i])
 
 # Hello
 
